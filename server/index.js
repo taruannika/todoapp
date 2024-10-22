bcaf93dfd5e5b80aa1dae034fda9b789c51534a8
HamalainenTaru <thamalainen1990@gmail.com>
2024-10-22 21:25:10 +0300

    running server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const app = require("./app");
+
+const { PORT } = require("./utils/config");
+const { info } = require("./utils/logger");
+
+app.listen(PORT, () => {
+  info(`Server running at port ${PORT}`);
+});
---
+++ server/utils/config.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+module.exports = { PORT };
