e499b9d11e00ad421114c2d069675a0350ec6475
HamalainenTaru <thamalainen1990@gmail.com>
2024-10-22 22:23:47 +0300

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Invalid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: function (value) {
+          return this.validator.isStrongPassword(value, {
+            minlength: 8,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 1,
+          });
+        },
+        message: "Password is not strong enought",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+userSchema.set("toJSON", {
+  transform: (document, user) => {
+    user.id = user._id.toString();
+    delete user._id;
+    delete user.__v;
+    delete user.password;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
